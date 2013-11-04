f4faca0b23dec9550dbe5f1ef379163a5190dd91
zhenghl <coolhuali@sina.com>
2013-11-04 16:31:46 +0800

    commit group controller

---
+++ Circle/Controllers/Group/GroupListViewController.h
@@ -0,0 +1,13 @@
+//
+//  GroupListViewController.h
+//  Circle
+//
+//  Created by admin on 13-11-4.
+//  Copyright (c) 2013年 icss. All rights reserved.
+//
+
+#import "BaseListViewController.h"
+
+@interface GroupListViewController : BaseListViewController
+
+@end
---
+++ Circle/Controllers/Group/GroupListViewController.m
@@ -0,0 +1,38 @@
+//
+//  GroupListViewController.m
+//  Circle
+//
+//  Created by admin on 13-11-4.
+//  Copyright (c) 2013年 icss. All rights reserved.
+//
+
+#import "GroupListViewController.h"
+
+@interface GroupListViewController ()
+
+@end
+
+@implementation GroupListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
